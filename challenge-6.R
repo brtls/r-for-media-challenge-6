0687e9b41348cc56709a7ecf6ef9a17500480bb0
Malte Bartels <maltechristian.bartels@haw-hamburg.de>
2021-04-26 22:57:05 +0200

    Bartels_challenge-6

--- challenge-6.R
+++ challenge-6.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 library(purrr)
+library(ggrepel)
 
 # Stelle den mitgegebenen Plot nach. Alle Daten dafür habt Ihr ja vorliegen!
 
@@ -12,18 +13,52 @@ library(purrr)
 # load their Nebentätigkeiten
 # nebeneinkuenfte <- …
 
+nebeneinkuenfte <- read.csv("data-ROsix.csv", encoding = "UTF-8") %>% 
+  janitor::clean_names()  %>%  
+  mutate(
+    name = str_split(name, ", ") %>% map_chr(~rev(.) %>% paste(collapse = " ")),
+    nebentaetigkeiten = nebentatigkeiten == "ja"
+  ) %>% 
+  select(-nebentatigkeiten, -partei)
+
+bundestag_2019 <- readRDS("bundestag_2019.rds")
+
+bundestag_2019 <- bundestag_2019 %>% 
+  left_join(nebeneinkuenfte)
+
 partei_farben <- list(
   "CDU" = "black",
-  "SPD" = "red",
-  "CSU" = "black",
-  "FDP" = "yellow",
-  "Grüne" = "green",
-  "Linke" = "violet",
+  "SPD" = "red", 
   "AfD" = "blue",
+  "FDP" = "yellow",
+  "Linke" = "violet",
+  "Grüne" = "green", 
+  "CSU" = "black",
   "fraktionslos" = "grey"
 )
 
-# data <- …
+# disabling scientific notation
+options(scipen = 999)
 
-# ggplot(data) + …
-  
+# plotting all the needed value with a dot plot
+ggplot(data = bundestag_2019, aes(x = lebensdaten, y = land, size = mindest_einkunfte_in_euro, color = fraktion)) +
+  geom_point(alpha = 0.5, position = "jitter") +
+  scale_color_manual(values = partei_farben) +
+  geom_label_repel(
+    show.legend = FALSE,
+    data = bundestag_2019 %>% 
+      filter(mindest_einkunfte_in_euro > 450000),
+    aes(label = name),
+    color = "black",
+    size = 3.5,
+    fill = "white", 
+  ) +
+  theme_minimal() +
+  labs(
+    title = "Abgeordnete und deren Nebeneinkünfte",
+    subtitle = "Wie viel Verdienen die Bundestagsabgeordneten neben ihrer Hauptberufung?",
+    x = "Geburtsjahr",
+    y = "Bundesland",
+    size = "Mindestnebeneinkünfte",
+    color = "Fraktion"
+  )
